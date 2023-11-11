510dd8764e32e6807eaf8453bbf1715747d99091
ThanksCheersHrng <sistertree321@yahoo.com>
2023-11-11 14:28:43 +0000

    "adds comments for clarity to milestone_2.py"

--- milestone_2.py
+++ milestone_2.py
@@ -1,15 +1,18 @@
+#Hangman 
+
 import random as rn 
 
+#Select a random target word 
 word_list = ["blueberry","strawberry","raspberry","peach","tomato"] 
 #print(word_list)
 #print(type(word_list))
-
 word = rn.choice(word_list)
 #print(word)
 
+#Input guess for a letter 
 guess = input("Enter a single letter: ")
 
-
+#Check guess is a single letter. 
 if len(guess) == 1 and guess.lower() in "abcdefghijklmnopqrstuvwxyz":
     print("Good guess!")
 else:
