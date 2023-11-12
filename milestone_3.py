273722c671b890ad1b277462da09f0f7b162da32
ThanksCheersHrng <sistertree321@yahoo.com>
2023-11-12 12:05:36 +0000

    updates milestone_3.py to describe check_guess and ask_for_input functions, and to call as a test.

--- milestone_3.py
+++ milestone_3.py
@@ -3,16 +3,25 @@ import random as rn
 word_list = ["blueberry","strawberry","raspberry","peach","tomato"] 
 word = rn.choice(word_list)
 
-while True: 
-    #Input guess for a letter 
-    guess = input("Enter a single letter: ")
-    #Check guess is a single letter. 
-    if guess.isalpha() and len(guess) == 1:
-        break
+#checking if guess in word
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+        print(f"Sorry, {guess} is not in the word. Try again.")
 
-if guess in word:
-    print("Good guess! {guess} is in the word.")
-else:
-    print("Sorry, {guess} is not in the word. Try again.")
+#asking for input and checking validity:
+def ask_for_input(): 
+    while True: 
+        #Input guess for a letter 
+        guess = input("Enter a single letter: ")
+        #Check guess is a single letter. 
+        if guess.isalpha() and len(guess) == 1:
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+    check_guess(guess)
+
+#call to check game play
+ask_for_input()
